93e1aedaa71d9158d39f64f9ad4e771e04d67400
Adam Floyd <rainbowfloyd911@gmail.com>
2021-01-20 18:43:45 -0500

    Add dummy finance data

---
+++ src/dummyData.js
@@ -0,0 +1,20 @@
+const dummyFinanceData = {
+	income: {
+		work: 500,
+		bitcoin: 250,
+		treeFarm: 50,
+		fiverr: 60
+	},
+	expenses: {
+		lights: 250,
+		water: 20,
+		internet: 50,
+		phone: 120,
+		carInsurance: 130
+	},
+	debts: {
+		car: 10000,
+		house: 50000,
+		studentLoan: 22000
+	}
+}
